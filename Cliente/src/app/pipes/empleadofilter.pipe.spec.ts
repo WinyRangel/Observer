f98f66bfb95628f3b9fa8aac6da2809a72e27d0f
WinyRangel <99991728+WinyRangel@users.noreply.github.com>
2023-11-28 22:17:58 -0600

    Delete Cliente directory

--- Cliente/src/app/pipes/empleadofilter.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { EmpleadofilterPipe } from './empleadofilter.pipe';
-
-describe('EmpleadofilterPipe', () => {
-  it('create an instance', () => {
-    const pipe = new EmpleadofilterPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
